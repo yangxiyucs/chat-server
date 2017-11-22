64bf49f930b3a286b7341e1971b2167004e2e5eb
hugo boss <yangxiyucs@gmail.com>
2017-11-22 10:01:17 +0000

    Summary
    This code is for scalable computing task1

---
+++ Server.java
@@ -0,0 +1,40 @@
+package Chat;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server{
+
+	
+
+	// 定义保存所有Socket的ArrayList
+	//public static ArrayList<ArrayList<Socket>> socketList = new ArrayList<ArrayList<Socket>>();
+	public static ArrayList<Socket> sockets = new ArrayList<Socket>();
+	public static ArrayList<String> Rooms=new ArrayList<String>();
+	
+	public static void main(String[] args) {  
+	       
+	        try {  
+	            ServerSocket ss=new ServerSocket(9999);  
+	         /*   Rooms.add("Room1");
+	            System.out.println("New Room has been created");
+	            
+	            System.out.println("Server is started");*/
+	            while (true) {  
+	                //此代码会阻塞，将一直等待别人的连接  
+	                Socket s=ss.accept();  
+	                sockets.add(s);  
+	                //每当客户端连接后启动一条ServerThread线程为该客户端服务  
+	                int id = (int) (Math.random());
+	                new Thread(new ServerThread(s,id)).start();  
+	            }   
+	              
+	        } catch (IOException e) {  
+	            e.printStackTrace();  
+	        }  
+	          
+	          
+	    }
+
+}
